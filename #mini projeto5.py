65f7cd83a9a35f5e1ace95065edd5fac50c58efe
meunome1234 <soaresmatheushenrique15@gmail.com>
2025-10-20 13:41:24 -0300

    mini projeto 5 parcial2

--- #mini projeto5.py
+++ #mini projeto5.py
@@ -16,6 +16,8 @@ r = '\033[0m' # reset
 #variaveis---------------
 taxa = None
 var_cdb = False
+ir = 0.00
+vartempo = 0
 #--------------------------------------------------
     #missão1: tipo de investimento
 #--------------------------------------------------
@@ -23,20 +25,20 @@ var_cdb = False
 #------- início da conversa com o usuário ---------
 print(f"SIMULADOR DE INVESTIMENTOS")
 print(f"Olá, vou te ajudar a simular as possibilidadesde investimentos{ql}{ql}")
-print(f"Pra começar, quero te dizer que as {B}taxas anuais{W} que estou utilizando são:")
-print(f"{B}IPCA{W}(inflação):{P} 5.53%{W}")
-print(f"{B}CDI{W}(Jurus){W}.....{P} 14.65%{W}")
-print(f"{B}Poupança{W}:......{P} 6.00%{W}{ql}")
+print(f"Pra começar, quero te dizer que as {B}taxas anuais{r} que estou utilizando são:")
+print(f"{B}IPCA{W}(inflação):{P} 5.53%{r}")
+print(f"{B}CDI{W}(Jurus){W}.....{P} 14.65%{r}")
+print(f"{B}Poupança{W}:......{P} 6.00%{r}{ql}")
 #----
 valor_investimento = input(f"Agora me informa o valor em reais que você quer investir:{G} R$ ")
 print(f"{r}Ok, registrei o valor do seu investimento.{ql}{ql}")
 
 #---------- opções de investimento ------------
 print(f"Essas são as opções de investimento que tenho sisponíveis para você:")
-print(f" [A]{B}CDB{W} valendo 100% do CDI, taxa final de {P} 14.65%{W}")
-print(f" [B]{B}CDB{W} valendo 110% do CDI, taxa final de {P} 16.12%{W}")
-print(f" [B]{B}CDB{W} valendo 120% do CDI, taxa final de {P} 17.58%{W}")
-print(f" [B]{B}LCA{W} valendo  95% do CDI, taxa final de {P} 13.92%{W}")
+print(f" [A]{B}CDB{W} valendo 100% do CDI, taxa final de {P} 14.65%{r}")
+print(f" [B]{B}CDB{W} valendo 110% do CDI, taxa final de {P} 16.12%{r}")
+print(f" [C]{B}CDB{W} valendo 120% do CDI, taxa final de {P} 17.58%{r}")
+print(f" [D]{B}LCA{W} valendo  95% do CDI, taxa final de {P} 13.92%{r}")
 print(f"{i}Obs.: Lembre que o CDB retém ir na fonte, enquanto LCA não.{r}")
 # -------------seleção da opção--------------------------
 opcao = input(f"{ql}Qual o investimento que você quer fazer? (A,B,C ou D){G}")
@@ -61,31 +63,51 @@ else:
     print(f"Opção inválida! Tente novamente.")
 #-------mensagem cdb -----
 if var_cdb == True:
-    print(f"{W}Como você escolheu um CDB vou te lembrar as taxas regressivas de iR:")
-    print(f"Até 6 messes:.....{P} 22.50%{W}")
-    print(f"Até 12 meses:.....{P} 20.00%{W}")
-    print(f"Até 24 meses:.....{P} 17.50%{W}")
-    print(f"acima de 24 meses:{P} 15.00%{W}")
+    print(f"{r}Como você escolheu um CDB vou te lembrar as taxas regressivas de iR:")
+    print(f"Até 6 messes:.....{P} 22.50%{r}")
+    print(f"Até 12 meses:.....{P} 20.00%{r}")
+    print(f"Até 24 meses:.....{P} 17.50%{r}")
+    print(f"acima de 24 meses:{P} 15.00%{r}")
 
 #------------------------------------------------
 #-------fim missão 1 ----------------------------
 #------------------------------------------------
     #missão 2: tempo e resultado 
-vartempo = input(f"Quanto tempo você gostaria de esparar para resgatar esse investimento?(em meses){B}")
+vartempo = int(input(f"Quanto tempo você gostaria de esparar para resgatar esse investimento?(em meses){B}"))
 print(f"{r}Ok, registrei o tempo para resgate.{ql}{ql}")
 
-#condicionais vartempo
-if vartempo <= "6":
+#condicionais vartempo (IR)
+if vartempo <= 6:
     ir = 22.5
-elif vartempo > "6" and vartempo <= "12":
+elif vartempo > 6 and vartempo <= 12:
     ir = 20.0
-elif vartempo > "12" and vartempo <= "24":
+elif vartempo > 12 and vartempo <= 24:
     ir = 17.5
 else:
     ir = 15.0
 
+#calculo taxa mensal
+taxa_mensal = ((1 + taxa/100) ** (1/12) - 1) *100
 #resposta----
 print(f"TAXAS UTILIZADAS")
-print(f"Taxa de IR aplicada.......{P} {ir}.:2F%{W}")
-print(f"taxa de rendimento anual..{P} {taxa}%.2F{W}")
-print(f"Taxa de rendimento mensal..{P} {(taxa/12):.2F}%{W}")
+print(f"Taxa de IR aplicada.......{P} {ir:.2f}%{r}")
+print(f"taxa de rendimento anual..{P} {taxa:.2F}%{r}")
+print(f"Taxa de rendimento mensal..{P} {taxa_mensal:.2F}%{r}{ql}")
+
+#calculos dos juros e resgate------------------
+rendimento_bruto = float(valor_investimento) * ((1 + (taxa/100)/12) ** vartempo - 1)#valor investido * a taxa dividido por ano e multiplicado pelo tempo - mês inicial
+valor_deduzido = rendimento_bruto * (ir / 100)
+resgate = float(valor_investimento) + rendimento_bruto - valor_deduzido
+lucro_total = rendimento_bruto - valor_deduzido
+
+#-----resultado-----
+print("RESULTADO")
+print(f"Valor investido:.......{G}R$ {float(valor_investimento):.2f}{r}")
+print(f"Rendendo pelo tempo de {B}{vartempo} meses{r}")
+print(f"Dedução do IR de:......{P}{ir:.2f}%{r}")
+print(f"Valor deduzido é de....{G}R$ {valor_deduzido:.2f}{r}")
+print(f"O resgate será de......{G}R$ {resgate:.2f}{r}")
+print(f"O lucro total será.....{G}R$ {lucro_total:.2f}{r}")
+
+#-------fim missão 2 ----------------------------
+#------------------------------------------------
