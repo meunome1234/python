059473a58a6cf0a3e25def4bbcf18509b71b2747
meunome1234 <soaresmatheushenrique15@gmail.com>
2025-11-13 17:59:30 -0300

    mini projeto5 v0.2

---
+++ funcoes_mini_projeto_5.py
@@ -0,0 +1,5 @@
+def inserir_pessoa():
+    nome = input("Digite seu nome")
+    salario = float(input("digite o seu salário"))
+    patrimonio = float(input("Digite seu patrimonio"))
+    return nome, salario, patrimonio
