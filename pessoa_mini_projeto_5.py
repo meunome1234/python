3657f32ef215fba3d6f370f4373f755900ccab7b
meunome1234 <soaresmatheushenrique15@gmail.com>
2025-11-13 05:31:35 -0300

    mini projeto 5 v inicial

---
+++ pessoa_mini_projeto_5.py
@@ -0,0 +1,6 @@
+class pessoa:
+    def __init__(self, nome, salario, patrimonio):
+        self.nome = nome
+        self.salario = salario
+        self.patrimonio = patrimonio
+#definir  nome como string, salario e patrimonio como float 
