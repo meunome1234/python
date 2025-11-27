1caf525a78aabe5eb994f377e4884ca2320fee8d
meunome1234 <soaresmatheushenrique15@gmail.com>
2025-11-27 17:46:34 -0300

    mp06

---
+++ mp06/src/main.py
@@ -0,0 +1,87 @@
+#cria texto
+with open("mini_projetro_5.txt", "w") as arquivo:#criar o arquivo
+    arquivo.write("cadastro de pessoas\n")
+
+#--formatação de texto--------
+G = '\033[32m' # green - verde
+P = '\033[1;35m' # purple - roxo 
+ql = "\n" #quebra de linha
+i = '\033[3m' # italico
+r = '\033[0m' # reset
+#-------------------------------------------
+#importar funções
+from funcoes_mini_projeto_5 import digitar_dados_pessoa
+from funcoes_mini_projeto_5 import mostrar_dados_pessoas
+from indices_mini_projeto_5 import taxa_mensal_rendimento
+from indices_mini_projeto_5 import gastos
+from funcoes_mini_projeto_5 import simular_mes_populacao
+#-------------------------------------------
+
+
+
+
+
+#programa principal
+def main():
+
+    pessoas = [] #cria lista vazia para armazenar pessoas
+
+    sair = False #variável de controle do while do menu
+
+
+    #menu de opções------------------------------------------------------------------------
+    while sair == False:
+        print(f"[SIMULADOR DE RELAÇÕES DE MERCADO]{ql}escolha uma opção:{ql}- "
+            f"[{P}d{r}]igitar dados de uma nova pessoa{ql}- "
+            f"[{P}m{r}]ostrar dados de nova pessoa{ql}- "
+            f"S[{P}i{r}]mular finanças mensais da população{ql}- "
+            f"[{P}s{r}]air{ql}")
+
+        opcao = input(f"opção: {P}")
+        print(r) #reseta a cor após a entrada da opção
+
+        if opcao == "d":
+            print(f"[INSERIR NOVA PESSOA]{ql}")
+            digitar_dados_pessoa(pessoas)
+            input(f"{ql}pressione enter...")#freiar o while
+            
+
+        elif opcao == "m":
+            print(f"[DADOS DAS PESSOAS]")
+            mostrar_dados_pessoas(pessoas)
+            input(f"{ql}pressione enter...")#freiar o while
+
+        elif opcao == "i":
+            print(f"[SIMULAR MÊS DA POPULAÇÃO]{ql}")
+            
+            avisos, pagamentos = simular_mes_populacao(pessoas, taxa_mensal_rendimento, gastos) #chama a função para simular o mês da população e captura os avisos 
+        
+
+            print(f"{ql}[RELATÓRIO DE PAGAMENTOS]{ql}")
+            for p in pagamentos:
+                print(p)
+
+            if avisos:
+                print(f"{ql}[RELATÓRIO DE AVISOS]{ql}")
+                for a in avisos:
+                    print(a)
+           
+
+            print(f"{i}Simulação de um mês concluída para todas as pessoas.{r}{ql}")
+            input(f"{ql}pressione enter...")#freiar o while
+        
+        
+        
+        elif opcao == "s":
+            sair = True
+            print(f"")
+
+        else:
+            print(f"{ql}Entrada inválida{ql}")
+            
+    #-------------------------------------------------------------------------------------
+#chama a função main
+if __name__ == "__main__":
+    main()
+
+#--------------------------------------------
---
+++ mp06/src/pessoa.py
@@ -0,0 +1,9 @@
+#cria a classe pessoa com os atributos nome, salario, patrimonio e conta_corrente
+
+class Pessoa:
+    def __init__(self, nome, salario, patrimonio,conta_corrente ):
+        self.nome = nome
+        self.salario = salario
+        self.patrimonio = patrimonio
+        self.conta_corrente = conta_corrente
+#--------------------------------------------
